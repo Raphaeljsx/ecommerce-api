de7b1133526dfed66e57cbce4da52c5c85c69ea0
RaphaelJSX <raphaelalvoliv@gmail.com>
2024-12-18 22:19:51 -0300

    Inserção do firebase e reformulação dos controllers

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { getFirestore } from "firebase-admin/firestore";
 
 type User = {
   id: number;
@@ -6,45 +7,46 @@ type User = {
   email: string;
 };
 
-let id = 0;
 const usuarios: User[] = [];
 
 export class usersControllers {
-  static getAll(req: Request, res: Response) {
-    res.send(usuarios);
+  static async getAll(req: Request, res: Response) {
+    const snapshot = await getFirestore().collection("users").get();
+    const users = snapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
+    res.send(users);
   }
 
-  static getById(req: Request, res: Response) {
-    const userId = Number(req.params.id);
-    usuarios.find((user) => user.id == userId)
-      ? res.send(usuarios.find((user) => user.id == userId))
-      : res.send({ message: "Usuário não encontrado" });
+  static async getById(req: Request, res: Response) {
+    const userId = req.params.id;
+    const user = await getFirestore().collection("users").doc(userId).get();
+    res.send({ id: user.id, ...user.data()});
   }
 
-  static create(req: Request, res: Response) {
+  static async create(req: Request, res: Response) {
     const user = req.body;
-    user.id = id++;
-    usuarios.push(user);
+    const userSalvo = await getFirestore().collection("users").add(user);
     res.send({
-      message: "Usuário criado com sucesso",
+      message: `Usuário criado com sucesso ${userSalvo.id}!`,
     });
   }
 
-  static update(req: Request, res: Response) {
-    const userId = Number(req.params.id);
-    const user = req.body;
-    const index = usuarios.findIndex((user) => user.id == userId);
-    usuarios[index].nome = user.nome;
-    usuarios[index].email = user.email;
+  static async update(req: Request, res: Response) {
+    const userId = req.params.id;
+    const user = req.body as User;
+    await getFirestore().collection("users").doc(userId).set({
+      nome: user.nome,
+      email: user.email,
+    });
     res.send({
       message: "Usuário atualizado com sucesso",
     });
   }
 
-  static delete(req: Request, res: Response) {
-    const userId = Number(req.params.id);
-    const index = usuarios.findIndex((user) => user.id == userId);
-    usuarios.splice(index, 1);
+  static async delete(req: Request, res: Response) {
+    const userId = req.params.id;
+    await getFirestore().collection("users").doc(userId).delete();
     res.send({
       message: "Usuário deletado com sucesso",
     });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { routes } from "./routes";
 
+const firebase_admin = require("firebase-admin");
+const serviceAccount = require("../firebase-adminsdk.json");
+ firebase_admin.initializeApp({
+  credential: firebase_admin.credential.cert(serviceAccount),
+});
+
 const app = express();
 const port = 8080;
 
